3161598dff72c9ae9544c7a9261480b6267de2ea
VacuouS <vacuous21@gmail.com>
2022-10-18 12:57:21 +0300

    DataBase

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import WorkWithDatabase
+import WorkWithDatabase as db
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    db.read_info_from_bd()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
