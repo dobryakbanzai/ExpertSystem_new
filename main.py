a08f04cfc3ce8ba5fbf9f056326e0c91f78ac327
dobryakbanzai <lalalandia228@gmail.com>
2022-10-18 13:57:17 +0300

    Added Classes.py Changed main

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import WorkWithDatabase as db
-
+import Classes as ClassWord
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,8 +14,20 @@ def print_hi(name):
     db.read_info_from_bd()
     db.writing_info_in_bd()
 
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+words = ['aaa', 'bbb', 'ccc']
+wordsClasses = [ClassWord.Word('aaa', 'aba', 'Nord'), ClassWord.Word('bbb', 'ass', 'ASsfa'), ClassWord.Word('ccc', 'asf', 'Afsaf')]
+
+a = process.extractOne("abb", words)
+print(a[0])
+
+b = [x for x in wordsClasses if x.wrd == a[0]]
+print(b[0].wrd, b[0].mean, b[0].region)
+# wordsClassesindex(a[0])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
