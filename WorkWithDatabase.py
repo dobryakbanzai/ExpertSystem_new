259f951cb18af939548fa1b83a30ff9cdf98ecb6
VacuouS <vacuous21@gmail.com>
2022-10-18 12:19:27 +0300

    Bone

---
+++ WorkWithDatabase.py
@@ -0,0 +1,10 @@
+# чтение с бд
+def read_info_from_bd():
+    print('Reading')
+# Создание Бд
+def create_bd():
+    print('creating')
+# Запись в бд
+def writing_info_in_bd():
+    print('writing')
+#
