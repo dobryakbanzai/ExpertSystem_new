38b6467e1ce24ecd4d90389ce69e2d57927afaa1
VacuouS <vacuous21@gmail.com>
2022-10-21 13:05:44 +0300

    WriteInfo

--- WorkWithDatabase.py
+++ WorkWithDatabase.py
@@ -1,52 +1,58 @@
 # чтение с бд
 import csv
 from collections import defaultdict
+from csv import DictWriter
 
 from Classes import Word
 
 
 def read_info_from_bd():
     columns = defaultdict(list)  # each value in each column is appended to a list
-
     with open('db.csv') as f:
-
         reader = csv.reader(f,delimiter=';')
         next(reader)
-
         for row in reader:
             for (i, v) in enumerate(row):
                 if(v!=''):
                     columns[i].append(v)
-
-
         mylist = []
-
         for i in range(0, len(columns[0])):
             mylist.append(Word(columns[0][i],columns[1][i],'Северный'))
 
         for i in range(0, len(columns[2])):
             mylist.append(Word(columns[2][i],columns[1][i],'Центральный'))
-
         for i in range(0, len(columns[4])):
             mylist.append(Word(columns[4][i],columns[1][i],'Южный'))
-
+    f.close()
 
     array=columns[0]+columns[2]+columns[4]
-
-    for x in range(len(mylist)):
-        print (mylist[x].getWrd())
     return array,mylist
+
 
 # Запись в бд
 def writing_info_in_bd(word, array,mylist):
-    if word.getRegion()=='Северный':
-        print()
-    if word.getRegion()=='Центральный':
-        print()
-    if word.getRegion()=='Южный':
-        print()
 
-    mylist.append(word.getWrd,word.getMean(),word.getRegion())
+    with open('db.csv', 'a', newline='') as f:
+        headersCSV = ['Северный-слово', 'Северный-значение', 'Центральный-слово', 'Центральный-значение', 'Южный-слово',
+                      'Южный-значение']
+        if word.getRegion()=='Северный':
+
+            add_info('Северный',word,f,headersCSV)
+            print('add '+ word.getMean())
+        if word.getRegion()=='Центральный':
+            add_info('Центральный',word, f, headersCSV)
+            print('add '+ word.getMean())
+        if word.getRegion()=='Южный':
+            add_info(word,f,headersCSV)
+            print('add '+ word)
+    f.close()
+    mylist.append(word)
     array.append(word)
     return array,mylist
+
+def add_info(region,word,f,headerCSV):
+    dictwriter_object = DictWriter(f, fieldnames=headerCSV,delimiter=';')
+    dict = {region+'-слово':word.getWrd(), region+'-значение':word.getMean()}
+    dictwriter_object.writerow(dict)
+
 #
--- main.py
+++ main.py
@@ -3,16 +3,20 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import WorkWithDatabase as db
-import Classes as ClassWord
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
+from Classes import Word
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    db.create_bd()
-    db.read_info_from_bd()
-    db.writing_info_in_bd()
+
+    all_words,words_info=db.read_info_from_bd()
+    example=Word('Мансур','Салихов','Центральный')
+    print(example.getMean())
+    all_words,words_info=db.writing_info_in_bd(example,all_words,words_info)
+    for x in range(len(words_info)):
+        print(words_info[x].getWrd())
+
 
 
 
@@ -20,14 +24,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-words = ['aaa', 'bbb', 'ccc']
-wordsClasses = [ClassWord.Word('aaa', 'aba', 'Nord'), ClassWord.Word('bbb', 'ass', 'ASsfa'), ClassWord.Word('ccc', 'asf', 'Afsaf')]
 
-a = process.extractOne("abb", words)
-print(a[0])
-
-b = [x for x in wordsClasses if x.wrd == a[0]]
-print(b[0].wrd, b[0].mean, b[0].region)
 # wordsClassesindex(a[0])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
