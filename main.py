166401b12855b4b6531771fc48b33b91042c80f4
dobryakbanzai <lalalandia228@gmail.com>
2022-10-25 11:14:49 +0300

    Added WordSearch.py Changed main

--- main.py
+++ main.py
@@ -4,26 +4,25 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import WorkWithDatabase as db
 from Classes import Word
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import WordSearch as ws
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-    all_words,words_info=db.read_info_from_bd()
-    example=Word('Мансур','Салихов','Центральный')
+    all_words, words_info = db.read_info_from_bd()
+    example = Word('Мансур', 'Салихов', 'Центральный')
     print(example.getMean())
-    all_words,words_info=db.writing_info_in_bd(example,all_words,words_info)
+    all_words, words_info = db.writing_info_in_bd(example, all_words, words_info)
     for x in range(len(words_info)):
         print(words_info[x].getWrd())
-
-
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    print(ws.get_first_5('баклон'))
 
 # wordsClassesindex(a[0])
 
